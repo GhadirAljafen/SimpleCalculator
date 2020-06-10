5d6a295af4eec93187a79aa1f55c02086d52efee
GhadirAljafen <57419469+GhadirAljafen@users.noreply.github.com>
2020-06-10 17:22:14 +0300

    Update IArthimaticOperation.cs

--- CalculatorManager/IArthimaticOperation.cs
+++ CalculatorManager/IArthimaticOperation.cs
@@ -7,48 +7,4 @@ public    interface IArthimaticOperation
         double DoOperation(double resultValue, double number);
     }
 
-
-    //abstract class Animal
-    //{
-    //    private string Name;
-
-    //    public void SetName(string name) { Name = name; }
-    //    public virtual string GetName() { return Name; }
-
-    //    private int NumberOfLegs;
-    //    public void SetNumberOfLegs(int numberOfLegs) { NumberOfLegs = numberOfLegs; }
-    //    public int GetNumberOfLegs() { return NumberOfLegs; }
-    //}
-
-    //class Dog : Animal
-    //{
-    //    public Dog()
-    //    {
-    //        SetNumberOfLegs(4);
-    //    }
-
-    //    public void DugHole()
-    //    {
-
-    //    }
-
-    //    public override string GetName()
-    //    {
-    //        base.GetName();
-    //        return "Puppy";
-    //    }
-    //}
-
-    //class Hawk: Animal
-    //{
-    //    public Hawk()
-    //    {
-    //        SetNumberOfLegs(2);
-    //    }
-    //    public void Fly()
-    //    {
-    //        //DO fly
-    //    }
-    //}
-
 }
